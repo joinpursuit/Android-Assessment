bc315c22304d87409109c981d5b411f4404bf5f2
lynksdomain <alanjholguin@gmail.com>
2020-03-20 10:31:32 -0400

    multiple test

--- app/src/test/java/com/example/AndroidTest/DSATest.java
+++ app/src/test/java/com/example/AndroidTest/DSATest.java
@@ -16,4 +16,28 @@ public class DSATest {
         output = dsaTest.multiply(a,b);
         assertEquals(6,output);
     }
+
+
+    @Test
+    public void testMultiplyingIntsTwo() {
+        int a = 235;
+        int b = 1;
+        int output;
+
+        DSA dsaTest = new DSA();
+        output = dsaTest.multiply(a,b);
+        assertEquals(235,output);
+    }
+
+    @Test
+
+    public void testMultiplyingIntsThree() {
+        int a = -5;
+        int b = 18;
+        int output;
+
+        DSA dsaTest = new DSA();
+        output = dsaTest.multiply(a,b);
+        assertEquals(-90,output);
+    }
 }
